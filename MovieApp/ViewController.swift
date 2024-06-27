05525ed654cc533b46b343306a16b42068f8396a
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-27 09:44:56 +0700

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Nguyen Quang Minh on 27/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
