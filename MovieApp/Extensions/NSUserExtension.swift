2ca8ea46661a823d5a0b8709dc5e79dc7f799485
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-09 00:30:49 +0700

    Add NSUserExtension and UILabelExtension files

---
+++ MovieApp/Extensions/NSUserExtension.swift
@@ -0,0 +1,8 @@
+//
+//  NSUserExtension.swift
+//  MovieApp
+//
+//  Created by Nguyen Quang Minh on 8/7/24.
+//
+
+import Foundation
---
+++ MovieApp/Extensions/UILabelExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UILabelExtension.swift
+//  MovieApp
+//
+//  Created by Nguyen Quang Minh on 8/7/24.
+//
+
+import UIKit
+
+class UILabelExtension: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
